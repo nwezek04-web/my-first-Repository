60e1a86a7b5c50dfff60e879ab534b479ac93826
nweze kingsley <nwezek04@gmail.com>
2025-11-06 15:37:04 +0100

    firdt commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// check the data type of the following
+  console.log(typeof 43235);
+  console.log(typeof "67" + 67); 
+  console.log(typeof house); 
+  console.log(typeof 0);
+  console.log(typeof "");
+  console.log(typeof null);
+  console.log(typeof undefined);
